909cd47aac478c8a54d15384d177b5a98f8bfdb3
rezalimena <64654679+rezalimena@users.noreply.github.com>
2024-09-30 22:28:19 +0700

    favicon

--- app.vue
+++ app.vue
@@ -138,11 +138,11 @@ useHead({
     },
   ],
   link: [
-    // {
-    //   rel: "icon",
-    //   type: "image/x-icon",
-    //   href: "/favicon.ico",
-    // },
+    {
+      rel: "icon",
+      type: "image/x-icon",
+      href: "/favicon.ico",
+    },
   ],
 });
 </script>
